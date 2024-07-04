dad0d460ea202bbb5cb0daf83c177df3bf218e28
Priya Kushte <priya.padgaonkar86@gmail.com>
2024-07-04 17:28:06 +0530

    Update ContentView.swift
    Updated code to display generated floyd's triangle on the same screen

--- FloydsTriangle/ContentView.swift
+++ FloydsTriangle/ContentView.swift
@@ -9,43 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var rowNo: String = ""
-
-        var body: some View {
-            VStack {
-                Text("Flyod's Triangle")
-                    .fontWeight(.bold)
-                    .font(.largeTitle)
-                
-                TextField(
-                        "Enter number of rows",
-                        text: $rowNo
-                    )
-                .textFieldStyle(.roundedBorder)
-                .padding()
-                
-                Button ("Generate") {
-                  // Action to perform
-                    drawTriangle(rowNo: rowNo)
-                 }
-                 .buttonStyle(.bordered)
-                
+    @State private var resultText: String = ""
+        @State private var errorMessage: String = ""
+    
+    var body: some View {
+        VStack {
+            Text("Flyod's Triangle")
+                .fontWeight(.bold)
+                .font(.largeTitle)
+            
+            TextField(
+                "Enter number of rows",
+                text: $rowNo
+            )
+            .textFieldStyle(.roundedBorder)
+            .padding()
+            
+            Button ("Generate") {
+                // Action to perform
+                drawTriangle(rowNo: rowNo)
+            }
+            .buttonStyle(.bordered)
+            
+            if !errorMessage.isEmpty {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+            }
+            
+            if !resultText.isEmpty {
+                Text(resultText)
+                    .multilineTextAlignment(.leading)
+                    .padding()
             }
         }
-        
+    }
+    
     func drawTriangle(rowNo: String) {
         
-        if let rowNoInt = Int(rowNo) {
+        if let rowNoInt = Int(rowNo), rowNoInt > 0 {
             var currentNumber = 1
+            var result = ""
             for i in 1...rowNoInt {
                 var row = ""
                 for _ in 1...i {
                     row += "\(currentNumber) "
                     currentNumber += 1
                 }
-                print(row.trimmingCharacters(in: .whitespaces))
+               result += row.trimmingCharacters(in: .whitespaces) + "\n"
+                resultText = result
+                errorMessage = ""
             }
         } else {
-            print("Invalid input")
+            resultText = ""
+            errorMessage = "Invalid input"
         }
     }
 }
