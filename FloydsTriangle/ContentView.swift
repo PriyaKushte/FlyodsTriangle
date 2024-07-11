ab650192aaa424b220d8df6180830d996ca3c676
Priya Kushte <priya.padgaonkar86@gmail.com>
2024-07-11 10:58:12 +0530

    Update ContentView.swift
    color and font size changes for the result text
    Added animation for smooth transition of the view when resultText is displayed on the screen
    minor UI adjustments

--- FloydsTriangle/ContentView.swift
+++ FloydsTriangle/ContentView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 struct ContentView: View {
     @State private var rowNo: String = ""
     @State private var resultText: String = ""
-        @State private var errorMessage: String = ""
+    @State private var errorMessage: String = ""
+    @State private var showResult: Bool = false
+    @State private var showError: Bool = false
     
     var body: some View {
         VStack {
             Text("Flyod's Triangle")
                 .fontWeight(.bold)
                 .font(.largeTitle)
+                .foregroundColor(.gray)
             
             TextField(
                 "Enter number of rows",
@@ -27,9 +30,13 @@ struct ContentView: View {
             
             Button ("Generate") {
                 // Action to perform
-                drawTriangle(rowNo: rowNo)
+                drawTriangle()
             }
             .buttonStyle(.bordered)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+            .padding(.bottom, 30)
             
             if !errorMessage.isEmpty {
                 Text(errorMessage)
@@ -40,30 +47,43 @@ struct ContentView: View {
                 Text(resultText)
                     .multilineTextAlignment(.leading)
                     .padding()
+                    .background(Color.green.opacity(0.1))
+                    .cornerRadius(8)
+                    .transition(.opacity)
+                    .animation(.easeInOut, value: 0.5)
+                    .font(.title)
+
             }
         }
     }
     
-    func drawTriangle(rowNo: String) {
-        
-        if let rowNoInt = Int(rowNo), rowNoInt > 0 {
-            var currentNumber = 1
-            var result = ""
-            for i in 1...rowNoInt {
-                var row = ""
-                for _ in 1...i {
-                    row += "\(currentNumber) "
-                    currentNumber += 1
+    func drawTriangle() {
+            if let rowNoInt = Int(rowNo), rowNoInt > 0 {
+                var currentNumber = 1
+                var result = ""
+                for i in 1...rowNoInt {
+                    var row = ""
+                    for _ in 1...i {
+                        row += "\(currentNumber) "
+                        currentNumber += 1
+                    }
+                    result += row.trimmingCharacters(in: .whitespaces) + "\n"
                 }
-               result += row.trimmingCharacters(in: .whitespaces) + "\n"
-                resultText = result
-                errorMessage = ""
+                withAnimation {
+                    resultText = result
+                    errorMessage = ""
+                    showResult = true
+                    showError = false
+                }
+            } else {
+                withAnimation {
+                    resultText = ""
+                    errorMessage = "Invalid input"
+                    showResult = false
+                    showError = true
+                }
             }
-        } else {
-            resultText = ""
-            errorMessage = "Invalid input"
         }
-    }
 }
 
 #Preview {
